5bc0e4b44fdba44d71741bf3e5ac4a68f35cedbe
Guillaume Savaton <guillaume@baierouge.fr>
2021-02-27 20:47:50 +0100

    Renaming

---
+++ fr/apropos.html.pm
@@ -0,0 +1,39 @@
+#lang pollen
+
+◊define-meta[lang]{fr}
+
+◊chapter{À propos de Sozi}
+
+◊section{Qui développe Sozi ?}
+
+Je m'appelle Guillaume Savaton. Je travaille comme ingénieur et enseignant-chercheur dans les domaines de l'électronique numérique, des systèmes embarqués et du traitement numérique du signal.
+
+Le développement d'un outil de ◊em{bureautique} en utilisant les technologies du web semble en décalage complet avec mes activités professionnelles.
+
+Sozi n'est pas un ◊em{produit} commercial. Il n'est développé ni par une entreprise, ni par une équipe de développeurs. Il s'agit d'un projet personnel que je développe sur mon temps libre. Pour cette raison, même si je fais de mon mieux pour aider les utilisateurs en difficulté, le temps que je peux consacrer à Sozi est limité.
+
+◊section{Pourquoi avoir développé Sozi ?}
+
+Au cours de ma carrière, j'ai réalisé de nombreuses présentations dans le cadre de mes activités d'enseignement et de recherche : cours, conférences scientifiques, réunions diverses. Des années 1990 aux années 2000, j'ai ainsi eu l'occasion d'utiliser différents outils de création de diaporamas : Microsoft PowerPoint, ◊link["https://www.openoffice.org/"]{OpenOffice}/◊link["https://fr.libreoffice.org/"]{LibreOffice} ◊link["https://fr.libreoffice.org/discover/impress/"]{Impress}, ◊link["http://www.ctan.org/tex-archive/macros/latex/contrib/beamer/doc/beameruserguide.pdf"]{Beamer}.
+
+Pendant ces dix années, le style de mes présentations a profondément évolué. J'ai commencé par adopter une pratique fréquente dans l'enseignement, qui consiste à projeter un diaporama pendant le cours et à en distribuer une version imprimée aux élèves. Il est alors tentant d'inclure beaucoup de texte dans les diapos pour que le document reste compréhensible à la lecture.
+
+Après réflexion, j'ai réalisé que mes diaporamas ne constituaient ni de bons supports de présentation, car trop verbeux, ni de bons ◊em{manuels de référence} par manque de structuration. Pour chacune des matières que j'enseigne, j'ai donc décidé de créer deux sortes de documents : des présentations où les diapos servent uniquement à illustrer un discours oral, et des ◊em{manuels} (PDF ou web) structurés comme des livres, auquel les élèves peuvent se référer pendant les activités pratiques.
+
+En 2009, ◊link["https://prezi.com/"]{Prezi} m'a été recommandé par un collègue qui comptait l'utiliser pour sa soutenance de thèse. L'idée m'a paru séduisante, mais les conditions d'utilisation ne me convenaient pas. En effet, à l'époque, Prezi proposait une plate-forme fermée dans laquelle l'édition, le stockage, la publication, et le visionnage des présentations devait obligatoirement passer par leurs services en ligne.
+
+La première version de Sozi a été écrite en un week-end, juste pour relever le défi de développer un outil de présentation sur le même principe que Prezi, mais en utilisant uniquement des formats ouverts et des logiciels libres. Il ne s'agit d'abord que d'un prototype très rudimentaire, et il s'écoulera presque un an avant que je me décide à en faire un véritable outil que je pourrais utiliser au quotidien.
+
+
+◊section{Pourquoi ce nom "Sozi" ?}
+
+Le nom "Sozi" est une déformation du mot français "Sosie", qui désigne une personne ayant une forte ressemblance avec une autre personne, au point qu'on peut les confondre. Le choix de ce nom semble suggérer que Sozi est une imitation fidèle de Prezi, mais ce n'est pas le cas.
+
+Certes, Sozi emprunte l'idée de base de Prezi, où la présentation s'organise comme l'exploration d'un plan, plutôt que comme une succession de diapos. Mais en pratique, n'ayant moi-même jamais utilisé Prezi, j'ai développé Sozi selon mes propres idées sans chercher à en reproduire toutes les fonctionnalités.
+
+En choisissant ce nom, j'ai également négligé le sens qu'il pouvait prendre dans d'autres langues. En Allemand, "Sozi" est un terme familier, voire désobligeant, qui désigne un partisan du socialisme ou de la social-démocratie. Je l'ai compris par hasard en découvrant que le hashtag ◊link["https://twitter.com/hashtag/sozi"]{#Sozi} sur Twitter donnait de très nombreux résultats en allemand, visiblement sans rapport avec mon logiciel. Pour les éventuels lecteurs germanophones de ce livre, je tiens donc à préciser que le choix du nom "Sozi" n'a pas de connotation politique.
+
+
+◊section{Que signifient les numéros de version de Sozi ?}
+
+À compléter.
