f0af07a1ba27733123cbbf19db4d315853f2b64c
Guillaume Savaton <guillaume@baierouge.fr>
2021-04-05 16:00:15 +0200

    en/about-sozi Section titles

--- src/en/about-sozi.html.pm
+++ src/en/about-sozi.html.pm
@@ -5,3 +5,56 @@
 ◊chapter{About Sozi}
 
 ◊warning{This chapter is still a draft. Please come back in a few days to read a more complete version.}
+
+◊section{What can you do with Sozi?}
+
+◊; TODO
+
+◊subsection{Sozi is not a drawing software}
+
+◊; TODO
+
+◊subsection{Sozi is not a general-purpose animation software}
+
+◊; TODO
+
+◊subsection{Sozi is not a website editor}
+
+◊; TODO
+
+
+◊section{Who develops Sozi?}
+
+◊; TODO
+
+
+◊section{Why did I create Sozi?}
+
+◊; TODO
+
+◊subsection{A presentation as a reference document}
+
+◊; TODO
+
+◊subsection{A presentation to illustrate a speech}
+
+◊; TODO
+
+◊subsection{A presentation as a map to explore}
+
+◊; TODO
+
+
+◊section{Why the name "Sozi"?}
+
+◊; TODO
+
+
+◊section{What do the Sozi version numbers mean?}
+
+◊; TODO
+
+
+◊section{What are the terms of use for Sozi?}
+
+◊; TODO
