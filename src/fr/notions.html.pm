ed9ee9a865482d6df5f11175525213471c608f0c
Guillaume Savaton <guillaume@baierouge.fr>
2021-12-21 21:58:12 +0100

    About SVG editors and the provided examples

--- src/fr/notions.html.pm
+++ src/fr/notions.html.pm
@@ -10,9 +10,19 @@
 
 Un aperçu des logiciels et des formats de données impliqués.
 
+◊warning{Ce livre étant consacré à Sozi, vous y trouverez peu d'informations sur les logiciels de dessin vectoriel et sur leur utilisation. Les exemples de présentations Sozi que je vous propose de construire s'appuieront toujours sur des documents SVG que j'aurai préparés pour vous.
+
+Je vous recommande cependant d'installer le logiciel ◊link["https://inkscape.org/fr/"]{Inkscape} et de commencer à vous familiariser avec son interface. Nous l'utiliserons pour examiner ou modifier les documents de base à chaque fois que ce sera utile à la compréhension du fonctionnement de Sozi.
+
+Lorsque vous serez en situation de créer votre propre contenu avec Inkscape, vous trouverez des informations détaillées dans ◊link["https://inkscape.org/fr/apprendre/"]{la rubrique Apprendre} de son site web.}
+
 ◊section{Dessin vectoriel}
 
 Différences entre images matricielles et vectorielles.
+
+◊subsection{Le format SVG}
+
+À compléter.
 
 ◊section{La métaphore de la caméra}
 
--- src/fr/premiere-presentation.html.pm
+++ src/fr/premiere-presentation.html.pm
@@ -6,4 +6,17 @@
 
 ◊warning{Ce chapitre est encore à l'état de brouillon. Revenez dans quelques jours pour lire une version plus aboutie.}
 
+La première présentation que nous allons construire ensemble s'intitule "Grandes idées". Pour vous mettre en appétit, je vous montre tout de suite le résultat que vous devez obtenir si vous suivez mes instructions.
+Cliquez n'importe où dans l'image ci-dessous et laissez la présentation se dérouler.
+
 ◊sozi["../assets/fr/grandes-idees/grandes-idees.sozi.html"]
+
+◊section{Télécharger et ouvrir le document de base}
+
+Cette présentation s'appuie sur un document SVG que j'ai créé pour vous avec le logiciel ◊link["https://inkscape.org/fr/"]{Inkscape}. Suivez le lien ci-dessous pour le télécharger et enregistrez-le à l'emplacement de votre choix. Je vous recommande de l'enregistrer dans un nouveau dossier ◊code{sozi-grandes-idees} qui regroupera tous les fichiers de cette présentation.
+
+◊itemize-icons{
+    ◊link["../assets/fr/grandes-idees/grandes-idees.svg" #:download #t]{◊item-icon["download"]◊code{grandes-idees.svg}}
+}
+
+Une fois le téléchargement terminé, vous pouvez ouvrir le document ◊code{grandes-idees.svg} avec Inkscape pour examiner son contenu et le modifier selon vos goûts. Dans la suite de ce chapitre, nous aurons l'occasion de faire des modifications afin d'observer les interactions entre Inkscape et Sozi.
