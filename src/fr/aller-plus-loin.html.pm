3a333e7b70dd6499dbb380a5a1ba5d80e02c42d3
Guillaume Savaton <guillaume@baierouge.fr>
2022-01-22 15:13:15 +0100

    fr: added stubs for chapters to write

---
+++ src/fr/aller-plus-loin.html.pm
@@ -0,0 +1,6 @@
+#lang pollen
+
+◊define-meta[lang]{fr}
+◊define-meta[toc]{here}
+
+◊toc{Aller plus loin}
---
+++ src/fr/utiliser-les-calques.html.pm
@@ -0,0 +1,7 @@
+#lang pollen
+
+◊define-meta[lang]{fr}
+
+◊chapter{Utiliser les calques}
+
+◊draft{Ce chapitre est encore à l'état de brouillon. Revenez dans quelques jours pour lire une version plus aboutie.}
