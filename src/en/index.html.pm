5a810ed983745a45f58d39a4efbaef8916080762
Guillaume Savaton <guillaume@baierouge.fr>
2023-04-09 18:01:07 +0200

    Removed link to Gofundme

--- src/en/index.html.pm
+++ src/en/index.html.pm
@@ -9,5 +9,5 @@
 ◊book-subtitle{The ultimate guide to create your presentations with Sozi}
 
 ◊draft{This book is work in progress. New chapters are added as soon as they are ready, without following a definite schedule. In the meantime, the official documentation for Sozi can still be found on the ◊link["https://sozi.baierouge.fr/"]{web site of the project}.
-You can help me dedicate some time to work on this book by ◊link["https://gofund.me/2f2b11db"]{participating in the ongoing fundraising campaign} or by ◊link["about-this-book.html#sec-2"]{donating via another service}.
+You can help me dedicate some time to work on this book by ◊link["about-this-book.html#sec-2"]{making a donation}.
 Thanks for your patience and your support.}
